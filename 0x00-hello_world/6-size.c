67baa38715626e56aff85768bdb1948903631042
nsohpeter <apiahpetertoch@gmail.com>
2023-06-15 17:38:39 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * prints the size of various types on the computer it is compiled and run on.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+printf("The size of a char: %zu byte(s)\n", typeof(char));
+printf("The size of an int: %zu byte(s)\n", typeof(int));
+printf("The size of long int: %zu byte(s)\n", typeof(long int));
+printf("The size of long long int: %zu byte(s)\n", typeof(long long int));
+printf("The size of float: %zu byte(s)\n", typeof(float));
+	
+return (0);
+}
