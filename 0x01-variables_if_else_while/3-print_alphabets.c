4c29e8a668f87804617260358384e2430af52170
nsohpeter <apiahpetertoch@gmail.com>
2023-06-16 15:26:29 -0700

    alphABETS

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,7 +2,8 @@
 
 /**
  * main - Entry point
- *  a program that prints the alphabet in lowercase, and then in uppercase, followed by a new line.
+ * a program that prints the alphabet in lowercase, and then in uppercase,
+ * followed by a new line.
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -16,6 +17,6 @@ for (caps = 'A'; caps <= 'Z'; caps++)
 {
 putchar(caps);
 }
- putchar('\n');
+putchar('\n');
 return (0);
 }
