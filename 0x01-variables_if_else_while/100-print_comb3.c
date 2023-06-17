26c27dcd771f6e7e3c776efd0b9ca46d1b952da8
nsohpeter <apiahpetertoch@gmail.com>
2023-06-16 17:41:49 -0700

    possible different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,11 +11,13 @@ int main(void)
 int tens, ones;
 for (tens = 0; tens < 9; tens++) 
 {
+
 for (ones = tens + 1; ones < 10; ones++)
 {
 putchar(tens + '0');
 putchar(ones + '0');
-if (tens != 8 || ones != 9) 
+
+if (tens != 8 || ones != 9)	
 { 
 putchar(',');
 putchar(' ');
