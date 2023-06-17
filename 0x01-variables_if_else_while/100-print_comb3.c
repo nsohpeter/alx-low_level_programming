e34abf2bb8eeaea234b302b0c0dfb0e2fdc0262e
nsohpeter <apiahpetertoch@gmail.com>
2023-06-16 17:31:05 -0700

    possible different combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * a program that prints all possible different combinations of two digits.,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int tens, ones;
+
+for (tens = 0; tens < 9; tens++) 
+{
+for (ones = tens + 1; ones < 10; ones++)
+{
+putchar(tens + '0');
+putchar(ones + '0');
+
+if (tens != 8 || ones != 9) 
+{ 
+    putchar(',');
+    putchar(' ');
+}
+}
+}
+
+putchar('\n');
+
+return (0);
+}
