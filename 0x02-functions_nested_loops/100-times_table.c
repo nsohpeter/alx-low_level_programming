bb1455eccc2bc99e0b7b6b216c6831c2b25ac237
nsohpeter <apiahpetertoch@gmail.com>
2023-06-21 09:06:33 -0700

    print nth times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,8 +8,6 @@
 void print_times_table(int n)
 {
 int i, j, k;
-
-
 if (n >= 0 && n <= 15)
 {
 for (i = 0; i <= n; i++)
@@ -24,7 +22,6 @@ _putchar(k + '0');
 else if (k < 10 && j != 0)
 {
 _putchar(',');
-_putchar(' ');
 _putchar(' ');
 _putchar(' ');
 _putchar(k + '0');
